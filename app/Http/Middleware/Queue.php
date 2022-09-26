4517e3308fbe93d67b15ef52ac7f6bbd19a51580
rameck88 <73326089+rameck88@users.noreply.github.com>
2022-09-26 17:15:31 -0300

    Fim da aula 3 da wdev

---
+++ app/Http/Middleware/Queue.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Middleware;
+
+class Queue{
+
+    /**
+     * Fila de middlewares a serem executados
+     * @var array
+     */
+    private $middlewares = [];
+
+    /**
+     * Função de execução do controlador
+     * @var Closure
+     */
+    private $controller;
+
+    /**
+     * Argumentos da função do controlador
+     * @var array
+     */
+    private $controllerArgs = [];
+
+    /**
+     * Método responsável por construir a classe de fila de middlewares
+     * @param array $middlewares
+     * @param Clousure $controller
+     * @param array $controllerArgs
+     */
+    public function __construct($middlewares,$controller,$controllerArgs)
+    {
+        $this->middlewares = $middlewares;
+        $this->controller = $controller;
+        $this->controllerArgs = $controllerArgs;
+    }
+}
